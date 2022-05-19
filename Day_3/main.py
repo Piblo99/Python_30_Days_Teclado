b03b6a476e26eed8feb391db73918a55d31059c4
Paul Hankins <42610498+Piblo99@users.noreply.github.com>
2022-05-19 23:17:37 +0000

    Day 3

---
+++ Day_3/main.py
@@ -0,0 +1,18 @@
+greeting = "Hello, world"
+print(f"{greeting}!")
+
+name = input("Please enter your name: ")
+print(f"Hello, {name}!")
+
+age = 29
+iam = "I am "
+
+iam29 = iam + str(age)
+print(iam29)
+
+title = "Joker"
+director = "Todd Phillips"
+release_year = 2019
+
+#Joker (2019), directed by Todd Phillips
+print(f"{title} ({release_year}), directed by {director}" )
