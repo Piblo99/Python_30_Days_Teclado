ae058f1047a5e9fa518ddef461aade7939b31660
Paul Hankins <42610498+Piblo99@users.noreply.github.com>
2022-05-19 22:45:48 +0000

    Day 1

---
+++ Day_1/main.py
@@ -0,0 +1,14 @@
+#age
+print(25)
+
+#days in 27 years
+print(365 * 27)
+
+#weeks in 27 years
+print(52 * 27)
+
+#months in 27 years
+print(12 * 27)
+
+#area of a circle with a radius of 5
+print(3.14 * (5 ** 2))
