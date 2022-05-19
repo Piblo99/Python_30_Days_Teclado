ae616466dd8d60234850d6d1d6a4258a201f72b8
Paul Hankins <42610498+Piblo99@users.noreply.github.com>
2022-05-19 22:58:37 +0000

    Day 2

---
+++ Day_2/main.py
@@ -0,0 +1,31 @@
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+
+print(name)
+print(age)
+
+name = 'sneed'
+print(name)
+
+"""
+hourly_wage = input("Please enter your hourly wage: ')
+
+prnt("Hourly wage: ")
+print(hourlywage)
+print("Hours worked: ")
+print(hours_worked)
+
+hours_worked = input("How many hours did you work this week? ")
+"""
+
+#The above code should be changed to the code below
+hourly_wage = input("Please enter your hourly wage: ")
+hours_worked = input("How many hours did you work this week? ")
+
+print("Hourly wage: ")
+print(hourly_wage)
+
+print("Hours worked: ")
+print(hours_worked)
+
+
