af699ae2591eedd2b7e68c3333f3ae4b3f758208
Paul Hankins <42610498+Piblo99@users.noreply.github.com>
2022-05-24 21:39:14 +0000

    Day 16

---
+++ Day_a16/main.py
@@ -0,0 +1,14 @@
+students = [
+	{"name": "Hannah", "grade_average": 83},
+	{"name": "Charlie", "grade_average": 91},
+	{"name": "Peter", "grade_average": 85},
+	{"name": "Rachel", "grade_average": 79},
+	{"name": "Lauren", "grade_average": 92}
+]
+
+students.sort(key=lambda student: student["name"])
+
+exp = lambda base, exponent: base ** exponent
+
+print(exp)
+
